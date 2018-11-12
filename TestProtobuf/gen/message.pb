a6d2c9c4ba3b3b72220e655fa0149305e187cf03
yhjsspz <88919046@qq.com>
2018-11-12 17:29:04 +0800

    初始化

---
+++ TestProtobuf/gen/message.pb
@@ -0,0 +1,9 @@
+
+
+bin/message.proto
+pb.Message"E
+C2M_EnterGame_10101
+Account (
+Sign (	
+Channel (	"
+M2C_GCEnterGame_10102
---
+++ TestProtobuf/pb3.bat
@@ -0,0 +1,19 @@
+@echo off
+
+del /q gen\*
+
+setlocal enabledelayedexpansion
+
+for %%i in (bin\*.proto) do (
+	
+	set old_name=%%i
+	set new_name=!old_name:proto=pb!
+	set new_name=!new_name:bin=gen!
+
+    protoc3 --descriptor_set_out !new_name! !old_name!
+
+)
+
+
+echo "finished"
+pause
